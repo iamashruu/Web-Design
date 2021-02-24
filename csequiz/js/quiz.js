204d66a5d427cbd9d3b6581849137afb19e1fcb2
unknown <ashrafuddin882@gmail.com>
2021-02-24 14:03:31 +0600

    Added Quiz Website!

---
+++ csequiz/js/quiz.js
@@ -0,0 +1,32 @@
+const correctAns = ['B','B','A','A'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+form.addEventListener('submit',e => {
+    e.preventDefault();
+
+    let score = 0;
+    const userAns = [form.q1.value,form.q2.value,form.q3.value,form.q4.value];
+
+    // check answer
+
+    userAns.forEach((answer,index) => {
+        if(answer === correctAns[index]) {
+            score += 25;
+        }
+    });
+
+    // show result
+    scrollTo(0,0);
+    result.classList.remove('d-none');
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if(output === score){
+            clearInterval(timer);
+        } else {
+            output++;
+        }
+    }, 10);
+});
