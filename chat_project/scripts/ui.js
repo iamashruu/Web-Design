541f402c05067973ea63895dd4992482ac6a4bd9
unknown <ashrafuddin882@gmail.com>
2021-04-20 00:54:27 +0600

    All fixed

--- chat_project/scripts/ui.js
+++ chat_project/scripts/ui.js
@@ -11,15 +11,17 @@ class ChatUI {
     }
 
     render(data){
-        const when = dateFns.distanceInWordsToNow(
-            data.created_at.toDate(),
-            {addSuffix: true}
-        );
+        // const when = dateFns.distanceInWordsToNow(
+        //     data.created_at.toDate(),
+        //     {addSuffix: true}
+        // );
+        const timer = data.created_at;
+        const when = new Date();
         const html = `
             <li class="list-group-item">
                 <span class="username">${data.username}</span>
                 <span class="message">${data.message}</span>
-                <div class="time">${when}</div>
+                <div class="time">${when.toLocaleTimeString(timer)} ${when.toDateString(timer)}</div>
             </li>
         `;
         
